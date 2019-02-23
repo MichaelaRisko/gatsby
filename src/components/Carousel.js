40849429c1a79280927974f34804c2d87e337db9
MichaelaRisko <risko.michaela@gmail.com>
2019-02-23 13:50:36 +0100

    changes after build

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,7 +4,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import hallinclusive from '../assets/images/hallinclusive.jpg'
-import homeinclusive from '../assets/images/Homeinclusive20.jpg'
+import homeinclusive from '../assets/images/HomeInclusive20.jpg'
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
