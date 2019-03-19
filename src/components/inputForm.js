39e6bd4d17f0af867d57bd617ddc4becd6d8eaaf
MichaelaRisko <risko.michaela@gmail.com>
2019-03-19 20:27:38 +0100

    [FEATURE] - changed form component to stateful component

--- src/components/inputForm.js
+++ src/components/inputForm.js
@@ -269,6 +269,7 @@ Kompletnú ponuku nájdete na
           <label className={this.state.agree ? "form-label form-label-checked" : "form-label"}>                
             <input type="checkbox" 
             name="agree"
+            value={this.state.agree}
             defaultChecked={this.state.agree}
             onChange={() => this.setState({ agree: !this.state.agree })} />
             Súhlasím so spracovaním osobných údajov v zmysle zákona o ochrane osobných údajov č. 18/2018 Z. z. v znení neskorších predpisov</label>
